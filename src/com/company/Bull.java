a716d403eb923a590799e86318564e30e6b87506
Shimi Sadaka <sh.sadaka@gmail.com>
2020-11-08 18:53:42 +0200

    task complete

--- src/com/company/Bull.java
+++ src/com/company/Bull.java
@@ -1,7 +1,96 @@
 package com.company;
-import java.util.Scanner;
+import java.util.Random;
+
+/* A class for generating random number and method for the game Bull'eyes */
+
+
 
 public class Bull {
 
+    private String randomNum;
+    private int NumOfGuess;
+    private boolean success = false;
+    private final int maxNum = 10000;
 
+
+    public Bull(){ // constructor for building the Bull object
+
+        this.randomNum = generateRandNum(); // generate a random number and initialize it.
+        this.NumOfGuess = 0;
+
+    }
+
+    /* private method for generating a number between 0 to 9999 */
+    private String generateRandNum(){
+
+        Random rand = new Random();
+
+        return Integer.toString(rand.nextInt(maxNum)); // returns String presentation of the rand Num
+    }
+
+    /* for checking the status of the game - success or not  */
+    public boolean isSuccess(){
+
+        return success;
+
+    }
+
+
+    /* the function that make the comparison and determine the result of the session
+    * @param number
+    * */
+    public boolean bullEyes(String number){
+
+        int bull = 0; // number of bull in the guess
+        int eye =0; // number of eyes in the guess
+        this.NumOfGuess++;
+
+        for (int i = 0 ; i < 4 ; i++){ // count the bulls
+
+            if (number.charAt(i) == this.randomNum.charAt(i))
+                bull++;
+        }
+        if (bull == 4) {
+
+            success(); // invoke the success function if the user guess the number */
+            success = true; // update the new status
+            return true;
+        }
+
+        for (int i = 0 ; i < 4 ; i++){ // count the eyes
+
+            for (int j = 0 ; j < 4 ; j++){
+
+                if (this.randomNum.charAt(j) == number.charAt(i)) {
+                    eye++;
+                    break;
+                }
+            }
+        }
+        eye = eye - bull; // minus the bull times
+        result(bull, eye);// invoke the meanwhile result function if the user did not guess the number
+
+        return false;
+
+    }
+    /* if the user guessed the number, then it print to the screen */
+    private void success(){
+
+        System.out.println("Success! You needed "+ NumOfGuess+" Guesses!");
+    }
+    /* if the user did not guess the number, then it print to the screen - try again and meanwhile results */
+    private void result(int bull, int eye){
+
+        System.out.println("Bull: "+bull+"\nEye: "+eye+"\nTry Again!\n");
+
+
+    }
+
+    /* for testing purposes */
+    @Override
+    public String toString() {
+
+        System.out.println(this.randomNum);
+        return this.randomNum;
+    }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,72 @@
 package com.company;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("\nHello World");
+
+        boolean play = true;
+        String userGuess;
+
+        while (play){
+
+
+            Bull newGame = new Bull();
+
+            while( ! newGame.isSuccess()){
+
+                boolean isNotNum = false;
+
+                Scanner scan = new Scanner(System.in);
+                System.out.println("Enter Your 4 digits Guess!");
+
+                userGuess = scan.nextLine(); // input a number from the user
+
+                if(userGuess.length()!= 4){ // check if its 4 digits
+                    System.out.println("Please enter 4 digits\n");
+                    continue;
+                }
+                for (int i=0 ; i<4 ; i++){
+                    if (userGuess.charAt(i) < 48 || userGuess.charAt(i) > 57) {// check if between ascii values
+                        System.out.println("Please enter just digits\n");
+                        isNotNum = true;
+                        break;
+
+                    }
+
+                }
+                if (isNotNum)
+                    continue;
+
+                newGame.bullEyes(userGuess);
+                newGame.toString();
+
+
+
+            }
+
+            while(true) {
+
+                String anotherShot;
+
+                Scanner scan = new Scanner(System.in);
+                System.out.println("Do you want another shot? y/n");
+
+                anotherShot = scan.nextLine();
+
+                if (anotherShot.equals("y"))
+                    break;
+                else if (anotherShot.equals("n")) {
+                    play = false;
+                    break;
+
+                }
+
+
+
+            }
+        }
+
+
     }
 }
